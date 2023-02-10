5a3fe825a7cad845118f200731ec62c6a0105582
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-09 19:52:26 -0500

    themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, ThemeProvider } from '@mui/material'
+import { darkTheme } from './darkTheme';
+import { lightTheme } from './lightTheme';
+import { useTheme } from './contexts/themeContext';
 import './App.css'
 
 function App() {
+  const { isDarkMode } = useTheme()
+
   return (
-    <Stack>
-      <Typography variant='h2'>KeepTyping</Typography>
-      <Stack sx={{m: 10}}>
-        <Typography variant='h5'>How to play:</Typography>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <Stack>
+        <Typography variant='h2'>KeepTyping</Typography>
+        <Stack sx={{ m: 10 }}>
+          <Typography variant='h5'>How to play:</Typography>
+        </Stack>
       </Stack>
-    </Stack>
+    </ThemeProvider>
   )
 }
 
