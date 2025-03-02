afc830307b75e838da8b8b3b3c4c89c696865de3
Eilaf Aljundi <elafjundi@gmail.com>
2025-03-01 21:14:28 -0500

    added icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 import { StyledButton } from '../styledComponents/StyledButton'
 import { useNavigate } from 'react-router-dom'
 import HowToModal from '../HowToModal'
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
+import ShapeLineIcon from '@mui/icons-material/ShapeLine'
+import CategoryIcon from '@mui/icons-material/Category'
 
 function Home() {
     const [isHTPModalOpen, setIsHTPModalOpen] = useState(false)
@@ -12,13 +15,12 @@ function Home() {
         <Stack spacing={10} justifyContent='center'>
             <Typography variant='h2' color='primary'>KeepTyping</Typography>
             <Stack spacing={10} direction='row' justifyContent='center'>
-                <StyledButton color='primary' variant='contained' onClick={() => { navigate('/play') }}>Quick Game</StyledButton>
-                <StyledButton color='primary' variant='contained' onClick={() => {  }}>Hard Game</StyledButton>
-                <StyledButton color='primary' variant='contained' onClick={() => {  }}>Categories Game</StyledButton>
+                <StyledButton color='primary' variant='contained' sx={{width: 1/3}} startIcon={<RocketLaunchIcon />} onClick={() => { navigate('/play') }}>Quick Game</StyledButton>
+                <StyledButton color='primary' variant='contained' sx={{width: 1/3}} startIcon={<ShapeLineIcon />} onClick={() => { }}>Hard Game</StyledButton>
+                <StyledButton color='primary' variant='contained' sx={{width: 1/3}} startIcon={<CategoryIcon />} onClick={() => { }}>Categories Game</StyledButton>
             </Stack>
             <Stack spacing={10} direction='row' justifyContent='center'>
-                <StyledButton color='primary' variant='contained' onClick={() => setIsHTPModalOpen(true)}>How to play</StyledButton>
-                <StyledButton color='primary' variant='contained' onClick={() => setIsHTPModalOpen(true)}>Sign In</StyledButton>
+                <StyledButton color='primary' variant='outlined' sx={{ color: 'text.main' }} onClick={() => setIsHTPModalOpen(true)}>How to play</StyledButton>
             </Stack>
             <HowToModal open={isHTPModalOpen} onClose={() => setIsHTPModalOpen(false)} />
         </Stack>
