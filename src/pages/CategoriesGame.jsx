30c18c54528bd2562708b3933bf8158ad1f4a907
Eilaf Aljundi <elafjundi@gmail.com>
2025-03-07 19:40:35 -0500

    categories game page

---
+++ src/pages/CategoriesGame.jsx
@@ -0,0 +1,144 @@
+import { Stack, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { StyledButton } from '../styledComponents/StyledButton'
+import { StyledInput } from '../styledComponents/StyledInput'
+import { useWordList } from '../contexts/wordListContext'
+import { useNavigate } from 'react-router-dom'
+import { useAnimalList } from '../contexts/animalsListContext'
+import { useCountryList } from '../contexts/countriesListContext'
+import { usePlantList } from '../contexts/plantsListContext'
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const time = 15
+const categories = ['Animal', 'Plant', 'Country']
+
+function CategoriesGame() {
+    const [usedWords, setUsedWords] = useState([])
+    const [timeLeft, setTimeLeft] = useState(time)
+    const [score, setScore] = useState(0)
+    const [letter, setLetter] = useState('')
+    const [inputWord, setInputWord] = useState("")
+    const [error, setError] = useState('')
+    const { wordList, setWordList } = useWordList()
+    const { animalsList, setAnimalList } = useAnimalList()
+    const { countriesList, setCountryList } = useCountryList()
+    const { plantsList, setPlanyList } = usePlantList()
+    const [chosenCategory, setChosenCategory] = useState('')
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setLetter(letters[Math.floor(Math.random() * letters.length)])
+        setChosenCategory(categories[Math.floor(Math.random() * categories.length)])
+        setScore(0)
+        setUsedWords([])
+        setError('')
+        setTimeLeft(time)
+    }, [])
+
+    useEffect(() => {
+        let timerId
+        if (timeLeft > 0) {
+            timerId = setInterval(() => {
+                setTimeLeft(timeLeft - 1)
+            }, 1000)
+        } else {
+            // navigate('/gameover', { state: { score: score } }) // Time ran out
+        }
+
+        return () => {
+            clearInterval(timerId)
+        }
+    }, [timeLeft])
+
+    const handleWordSubmit = event => {
+        if (event.keyCode === 13) { // Enter key was pressed
+            switch (chosenCategory.toUpperCase()) {
+                case 'ANIMAL':
+                    if (animalsList.includes(inputWord.toUpperCase())) {
+                        if (inputWord.charAt(0).toUpperCase() == letter) {
+                            if (usedWords.includes(inputWord)) {
+                                setError('Already used this word!')
+                                return
+                            }
+                            setError('')
+                            setUsedWords([...usedWords, inputWord])
+                            setScore(prev => prev + 1)
+                            setInputWord('')
+                            setTimeLeft(time)
+                            setLetter(inputWord.charAt(inputWord.length - 1).toUpperCase())
+                            setChosenCategory(categories[Math.floor(Math.random() * categories.length)])
+                        } else {
+                            setError('Word must start with ' + letter)
+                        }
+                    } else {
+                        setError('Not an animal :/')
+                    }
+                    break;
+                case 'COUNTRY':
+                    if (countriesList.includes(inputWord.toUpperCase())) {
+                        if (inputWord.charAt(0).toUpperCase() == letter) {
+                            if (usedWords.includes(inputWord)) {
+                                setError('Already used this word!')
+                                return
+                            }
+                            setError('')
+                            setUsedWords([...usedWords, inputWord])
+                            setScore(prev => prev + 1)
+                            setInputWord('')
+                            setTimeLeft(time)
+                            setLetter(inputWord.charAt(inputWord.length - 1).toUpperCase())
+                            setChosenCategory(categories[Math.floor(Math.random() * categories.length)])
+                        } else {
+                            setError('Word must start with ' + letter)
+                        }
+                    } else {
+                        setError('Not a country :/')
+                    }
+                    break;
+                case 'PLANT':
+                    if (plantsList.includes(inputWord.toUpperCase())) {
+                        if (inputWord.charAt(0).toUpperCase() == letter) {
+                            if (usedWords.includes(inputWord)) {
+                                setError('Already used this word!')
+                                return
+                            }
+                            setError('')
+                            setUsedWords([...usedWords, inputWord])
+                            setScore(prev => prev + 1)
+                            setInputWord('')
+                            setTimeLeft(time)
+                            setLetter(inputWord.charAt(inputWord.length - 1).toUpperCase())
+                            setChosenCategory(categories[Math.floor(Math.random() * categories.length)])
+                        } else {
+                            setError('Word must start with ' + letter)
+                        }
+                    } else {
+                        setError('Not a plant :/')
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    return (
+        <Stack spacing={10} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Stack spacing={5}>
+                <Typography variant='h5'>Score: {score}</Typography>
+                <Stack direction='row' spacing={2}>
+                    <Typography variant='h3'>Type {chosenCategory === 'Plant' || chosenCategory === 'Country' ? 'a' : 'an'} </Typography>
+                    <Typography variant='h3' color='primary'>{chosenCategory.toUpperCase()}</Typography>
+                    <Typography variant='h3'>name that includes the letter</Typography>
+                </Stack>
+                <Typography variant='h2' color='primary'>{letter}</Typography>
+            </Stack>
+            <StyledInput type="text" value={inputWord} InputLabelProps={{ shrink: true, }} variant="standard" autoFocus sx={{ width: '50%' }} onChange={e => setInputWord(e.target.value)} onKeyDown={handleWordSubmit} inputProps={{ style: { fontSize: 40 }, autoComplete: 'off' }} />
+            <Typography variant='h6' color='red'>{error}</Typography>
+            <StyledButton color='error' variant='outlined' sx={{ width: '20%', color: 'text.main' }} onClick={() => { navigate(-1) }}>Back</StyledButton>
+            <p className='timer'>{timeLeft}</p>
+        </Stack>
+    )
+}
+
+export default CategoriesGame
