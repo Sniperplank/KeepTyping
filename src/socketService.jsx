56b636973b198629fdd1e4b326d1455ef8fbe1c0
Eilaf Aljundi <elafjundi@gmail.com>
2025-03-23 01:27:03 -0400

    ++

--- src/socketService.jsx
+++ src/socketService.jsx
@@ -1,16 +1,21 @@
 import { io } from 'socket.io-client';
 
-// Create a singleton socket instance
 const socket = io("https://keep-typing-server.onrender.com/", {
     transports: ['websocket'],
     reconnection: true,
     autoConnect: true
-});
+})
+
+// const socket = io("http://localhost:3000", {
+//     transports: ['websocket'],
+//     reconnection: true,
+//     autoConnect: true
+// })
 
 // Add debugging to track socket connections
 socket.on('connect', () => {
-    console.log('Connected to server with ID:', socket.id);
-});
+    console.log('Connected to server with ID:', socket.id)
+})
 
 // Export the socket instance
 export default socket;
